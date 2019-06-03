da4713a88fb2f3f676ac7e9ce2ca7f817ecf9d62
Lukas <lukas.becker.lb@gmail.com>
2019-06-03 20:18:17 +0200

    Edited code to eliminate warnings

--- app/src/main/java/com/example/virtebahelper/MainActivity.java
+++ app/src/main/java/com/example/virtebahelper/MainActivity.java
@@ -5,29 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.text.method.ScrollingMovementMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView;
-    Button button1;
-    String data; // a var to save the read data to
+    private TextView textView;
+    private Button button1;
+    private String data; // a var to save the read data to
 
-    String [] codesComplete = new String[156] ;   //A String array that saves each read line, Length=number of lines -1
-    String [] code =new String[156]; //the number of the diagnosis
-    String [] diag = new String[156]; //the diagnosis
+    private final String [] codesComplete = new String[156] ;   //A String array that saves each read line, Length=number of lines -1
+    private final String [] code =new String[156]; //the number of the diagnosis
+    private final String [] diag = new String[156]; //the diagnosis
 
-    InputStream stream; //a stream reader that reads a text file with all the diagnosis inside
-    BufferedReader reader;
+    private InputStream stream; //a stream reader that reads a text file with all the diagnosis inside
+    private BufferedReader reader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,11 +58,12 @@ public class MainActivity extends AppCompatActivity {
                 {
                     textView.append(code[i] + ":" + diag[i] + "\n");//append the read data to the TextView FOR DEBUGGING
                 }
+
             }
         });
 
     }
-    void readFunction() //read function, necessary because of anonymous inner class in thread
+    private void readFunction() //read function, necessary because of anonymous inner class in thread
     {
         if (stream != null) {
             try {
